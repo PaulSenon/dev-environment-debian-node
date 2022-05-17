df6e28f4b9a912f254cbdd76ec3f9c19341b171f
Paul Senon <paul.senon@euronews.com>
2022-05-17 20:23:14 +0200

    fix express template

--- templates/express/src/index.js
+++ templates/express/src/index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('/api/message', (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.json({
     message: 'Hello World!'
   });

774971b2a649701aaf8b473d82b2dcda2e0272fb
Paul Senon <paul.senon@euronews.com>
2022-05-17 20:25:49 +0200

    fix template node

--- templates/node/src/index.js
+++ templates/node/src/index.js
@@ -1,17 +1,1 @@
-const {parse} = require('node-html-parser');
-
-const html = '<p>a</p>  <p>b</p>  <div><p>1</p> <p>2</p> <p>3</p></div> <p>c</p> <p>d</p> <p>e</p>'
-const replacement = '<span>HELLOOOOOOOOOOOOO</span>';
-
-function insertHtml(htmlSource, htmlElement, position = 0) {
-  const dom = parse(`${htmlSource}`);
-  try{
-    dom.querySelector(`:root > p:nth-of-type(${position})`)
-      ?.insertAdjacentHTML('afterend', htmlElement);
-  } catch(err) {
-    console.error('Failled to parse html:', err);
-  }
-  return dom.toString();
-}
-
-console.log(insertHtml(html, replacement, 3));
+console.log('Hello World');
